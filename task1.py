b1397031f1bc8aa2db46f55bfece96b34b7b05d9
brandon0341 <itsmebrandon4@gmail.com>
2024-10-10 08:53:02 +0800

    place

---
+++ task1.py
@@ -0,0 +1,26 @@
+
+
+
+names = [
+['Alice', 'bob', 'Charlie'],
+['davin', 'Eve', 'Frank'],
+['Grace', 'Heidi', 'Ivan'],
+['Grace', 'Ken', 'laura']
+]
+
+Alice_found = False
+
+
+for list in names:
+    if "Alice" in list:
+        list.remove("Alice")
+        Alice_found = True     
+        
+
+if Alice_found:
+    new_name = input("alice not found. Place enter new Name")
+    names[0].append(new_name)
+
+print(names)
+
+
